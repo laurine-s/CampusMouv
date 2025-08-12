ec604eb99fda2901222a5449ffd817f1d4c400eb
jminel2025 <jonathan.minel2025@campus-eni.fr>
2025-08-12 10:09:00 +0200

    création des entités

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateHeureDebut = null;
+
+    #[ORM\Column]
+    private ?int $duree = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateLimiteInscription = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbInscriptionMax = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbInscriptionMin = null;
+
+    #[ORM\Column]
+    private ?int $nbInscrits = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $infos = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeImmutable
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(\DateTimeImmutable $dateHeureDebut): static
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateLimiteInscription(): ?\DateTimeImmutable
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    public function setDateLimiteInscription(\DateTimeImmutable $dateLimiteInscription): static
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+
+        return $this;
+    }
+
+    public function getNbInscriptionMax(): ?int
+    {
+        return $this->nbInscriptionMax;
+    }
+
+    public function setNbInscriptionMax(?int $nbInscriptionMax): static
+    {
+        $this->nbInscriptionMax = $nbInscriptionMax;
+
+        return $this;
+    }
+
+    public function getNbInscriptionMin(): ?int
+    {
+        return $this->nbInscriptionMin;
+    }
+
+    public function setNbInscriptionMin(?int $nbInscriptionMin): static
+    {
+        $this->nbInscriptionMin = $nbInscriptionMin;
+
+        return $this;
+    }
+
+    public function getNbInscrits(): ?int
+    {
+        return $this->nbInscrits;
+    }
+
+    public function setNbInscrits(int $nbInscrits): static
+    {
+        $this->nbInscrits = $nbInscrits;
+
+        return $this;
+    }
+
+    public function getInfos(): ?string
+    {
+        return $this->infos;
+    }
+
+    public function setInfos(string $infos): static
+    {
+        $this->infos = $infos;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
