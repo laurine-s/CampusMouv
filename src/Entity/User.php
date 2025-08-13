ba316e67778e0a97b64a214679c51929279c3ff7
laurine-s <laurine.suss@gmail.com>
2025-08-13 16:48:17 +0200

    Gestion des rôles en cours Création de l'enum, modification de l'entité, intégration dans security.yaml (certaines routes sont commentées pour ne pas perturber  les autres développements)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Enum\Role;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -32,7 +33,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var list<string> The user roles
      */
-    #[ORM\Column]
+    #[ORM\Column(type: 'json')]
     private array $roles = [];
 
     /**
@@ -117,7 +118,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = Role::PARTICIPANT->value;
 
         return array_unique($roles);
     }
@@ -125,7 +126,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @param list<string> $roles
      */
-    public function setRoles(array $roles): static
+    public function setRoles(array $roles): self
     {
         $this->roles = $roles;
 
