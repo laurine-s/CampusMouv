5c15ed10f92cd69c9df91b7007bbb650af88a83f
Laurine Süss <laurine.suss@gmail.com>
2025-08-17 18:30:53 +0200

    Refactorisation de la fonctionnalité photo de profil en intégrant le code métier en service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -31,40 +31,21 @@ final class UserController extends AbstractController
 
         if ($form->isSubmitted()) {
             $photoFile = $form->get('photo')->getData();
+            $uploadPhoto = [];
 
-             // Vérification erreur PHP avant validation Symfony pour la taille des fichiers
-            if ($photoFile && !$photoFile->isValid()) {
-                if ($photoFile->getError() === UPLOAD_ERR_INI_SIZE) {
-                    $this->addFlash('danger', 'Votre photo doit faire moins de 2M.');
-                } else {
-                    $this->addFlash('danger', 'Une erreur est survenue lors du téléchargement du fichier.');
-                }
-                return $this->redirectToRoute('profil');
-            }
-
-
-
-            // Vérification manuelle du format
             if ($photoFile) {
-                $finfo = new \finfo(FILEINFO_MIME_TYPE);
-                $mimeType = $finfo->file($photoFile->getPathname());
-                $allowedMimeTypes = ['image/jpeg', 'image/png'];
+                $uploadPhoto = $cloudinaryService->uploadPhoto($photoFile);
 
-                if (!in_array($mimeType, $allowedMimeTypes, true)) {
-                    $this->addFlash('danger', 'Seuls les fichiers JPEG ou PNG sont autorisés.');
+                if (!$uploadPhoto['success']) {
+                    $this->addFlash('danger', $uploadPhoto['error']);
                     return $this->redirectToRoute('profil');
                 }
             }
 
             if ($form->isValid()) {
 
-                if ($photoFile) {
-                    // On upload la photo sur Cloudinary
-                    $result = $cloudinaryService->upload($photoFile->getPathname());
-
-                    // on obtient l'URL publique est accessible via $result['secure_url']
-                    $user->setPhoto($result['secure_url']);
-                }
+                // on transmet l'url au user
+                $user->setPhoto($uploadPhoto['url']);
 
                 $em->persist($user);
                 $em->flush();
@@ -76,7 +57,6 @@ final class UserController extends AbstractController
             foreach ($form->getErrors(true) as $error) {
                 $this->addFlash('danger ', $error->getMessage());
             }
-
         }
 
         return $this->render('user/profil.html.twig', [
--- src/Service/CloudinaryService.php
+++ src/Service/CloudinaryService.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace App\Service;
 
 use Cloudinary\Api\ApiResponse;
 use Cloudinary\Api\Exception\ApiError;
 use Cloudinary\Cloudinary;
+use phpDocumentor\Reflection\Types\Array_;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class CloudinaryService
 {
     private Cloudinary $cloudinary;
+    private const DEFAULT_ALLOWED_FORMATS = ['image/jpeg', 'image/png'];
 
     public function __construct(string $cloudName, string $apiKey, string $apiSecret)
     {
@@ -30,4 +34,80 @@ class CloudinaryService
     {
         return $this->cloudinary->uploadApi()->upload($filePath);
     }
+
+    /**
+     * @throws ApiError
+     */
+    public function uploadPhoto(UploadedFile $photoFile)
+    {
+
+        // Vérification des erreurs PHP avant validation Symfony pour la taille des fichiers
+        $chekPhpError = $this->checkPHPErrors($photoFile);
+        if (!$chekPhpError['success']) {
+            return $chekPhpError;
+        }
+
+        // Vérification manuelle du format
+        $checkFormat = $this->checkFormat($photoFile, self::DEFAULT_ALLOWED_FORMATS);
+        if (!$checkFormat['success']) {
+            return $checkFormat;
+        }
+
+        // On upload la photo sur Cloudinary
+        $result = $this->upload($photoFile->getPathname());
+
+        // on obtient l'URL publique via $result['secure_url']
+        return [
+            'success' => true,
+            'url' => $result['secure_url'],
+        ];
+
+    }
+
+
+    public function checkPHPErrors(UploadedFile $photoFile): array
+    {
+
+        if (!$photoFile->isValid()) {
+
+            if ($photoFile->getError() === UPLOAD_ERR_INI_SIZE) {
+                return [
+                    'success' => false,
+                    'error' => 'Votre photo doit faire moins de 2M.',
+                ];
+            } else {
+                return [
+                    'success' => false,
+                    'error' => 'Une erreur est survenue lors du téléchargement du fichier.',
+                ];
+            }
+        }
+
+        return [
+            'success' => true,
+            'error' => null,
+        ];
+    }
+
+
+    public function checkFormat(UploadedFile $photoFile, array $formats): array
+    {
+
+        $finfo = new \finfo(FILEINFO_MIME_TYPE);
+        $photoFormat = $finfo->file($photoFile->getPathname());
+        if (!in_array($photoFormat, $formats, true)) {
+            return [
+                'success' => false,
+                'error' => 'Seuls les fichiers JPEG ou PNG sont autorisés.',
+            ];
+        }
+
+        return [
+            'success' => true,
+            'error' => null,
+        ];
+
+    }
+
+
 }
