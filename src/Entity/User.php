8560d625b07a8db669c006ad7e105f754dc8eaab
rfboulier <pro.boulier.romane@gmail.com>
2025-08-18 16:18:26 +0200

    chore(mail): configuration de Papercut pour l'envoi des emails en développement
     - Ajout de la configuration SMTP locale pour Papercut.
     - Tests des envois d'emails (mot de passe oublié).

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,14 +76,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\JoinColumn(nullable: true)]
     private ?Promo $promo = null;
 
-    #[ORM\Column(length: 100, unique:true, nullable: true)]
+    #[ORM\Column(length: 180, unique:true, nullable: true)]
     private ?string $pseudo = null;
-
-    #[Gedmo\Slug(fields: ['prenom', 'nom'], updatable: true, unique: true)]
-    //on passe le slug comme nullable au niveau PHP mais reste unique en DB
-    //ça permettra de ne pas avoir une erreur lors du chargement de fixtures
-    #[ORM\Column(length: 150, unique: true, nullable: true)]
-    private ?string $slug = null;
 
     public function __construct()
     {
@@ -302,6 +296,4 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-
-    public function getSlug(): string { return $this->slug; }
 }
