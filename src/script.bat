d26e3c2ee2c61f4519a3c644646fdf24010558d0
jminel2025 <jonathan.minel2025@campus-eni.fr>
2025-08-20 16:52:21 +0200

    chore(scheduler): modification du script pour exécuter le scheduler

--- src/script.bat
+++ src/script.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d D:\GitHub\CampusMouv
-d:\wamp64\bin\php\php8.3.14\php.exe bin\console app:backfill-sortie-datefin --also-archive
+start "" symfony serve
+php bin/console app:hourly-scheduler
